98979170e84a7daaab7b17e8f1ebcfc73f242a7f
Rob Antonisse <robantonisse@gmail.com>
2017-11-26 22:36:14 +0100

    start4

--- Licht/Sketch1/WS2812-Testen.ino
+++ Licht/Sketch1/WS2812-Testen.ino
@@ -6,10 +6,18 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-
+	pinMode(13, OUTPUT);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
   //dit is een test van de repos
+	if (digitalRead(13) == HIGH) {
+		digitalWrite(13, LOW);
+	}
+	else {
+		digitalWrite(13, HIGH);
+	}
+	delay(50);
+
 }
--- Licht/Sketch1/__vm/.Sketch1.vsarduino.h
+++ Licht/Sketch1/__vm/.Sketch1.vsarduino.h
@@ -12,6 +12,7 @@
 #define _VSARDUINO_H_
 #define __AVR_ATmega328p__
 #define __AVR_ATmega328P__
+#define _VMDEBUG 1
 #define F_CPU 16000000L
 #define ARDUINO 10801
 #define ARDUINO_AVR_UNO
@@ -65,5 +66,5 @@ typedef void *__builtin_va_list;
 #define F(string_literal) ((const PROGMEM char *)(string_literal))
 #undef PSTR
 #define PSTR(string_literal) ((const PROGMEM char *)(string_literal))
-#include "Sketch1.ino"
+#include "WS2812-Testen.ino"
 #endif
